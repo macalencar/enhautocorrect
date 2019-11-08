b2d2378cf4cb03d01af18952c3b79d21ed0c65da
Márcio Alencar <macalencar@gmail.com>
2019-11-08 00:20:50 -0400

    Agregate functions in init files
    1. Functions in dictionary_manager.py were appended to the  main file
    2. English dictionary updated
    3. Add/Remove words created

--- __init__.py
+++ __init__.py
@@ -15,19 +15,20 @@ https://github.com/fifimajster/autocorrect/
 Customized by: Márcio Alencar
 https://github.com/macalencar/enhautocorrect/
 
-"""
+1 - Remove item from dictionary
+2 - Store pruned dictionary
+3 - Thread/Multiprocessing tasks
 
+"""
 import json
 import re
 import tarfile
 import os
 from contextlib import closing
-
 from enhautocorrect.constants import word_regexes
 from enhautocorrect.typos import Word
 
 PATH = os.path.abspath(os.path.dirname(__file__))
-
 
 def load_from_tar(archive_name, file_name='word_count.json'):
     """ Decompress and load the frequency dictionary """
@@ -35,6 +36,51 @@ def load_from_tar(archive_name, file_name='word_count.json'):
         with closing(tarf.extractfile(file_name)) as file:
             return json.load(file)
 
+def get_words(filename, lang):
+    """ Gets the words from input file  based on word_regexes
+        associated to the language """
+    word_regex = word_regexes[lang]
+    #alphabeth_regex = r'(\.|^|<|"|\'|\(|\[|\{\s*)' + word_regexes[lang] # '(\.|^|<|"|\'|\(|\[|\{\s*)'
+    count_words=0
+    with open(filename) as file:
+        for line in file:
+            #line = re.sub(alphabeth_regex, '', line.lower())
+            for word in re.findall(word_regex, line):
+                if word.strip():
+                    yield word
+
+def parse(words):
+    """ Stores the words frequency """
+    counts = dict()
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+    return counts
+
+def save_dictionary(freq_dict, dict_file, out_filename='word_count.json'):
+    with open(out_filename, 'w') as outfile:
+        json.dump(freq_dict, outfile)
+    with tarfile.open(dict_file, "w:gz") as compressed_file:
+        compressed_file.add(out_filename)
+    os.remove(out_filename)        
+
+def count_words(src_filename, lang, update=True):
+    """ Analyze the input file based on the language and
+        create a compressed package for further usage """
+    words = get_words(src_filename, lang)
+    counts = parse(words)    
+    freq_dict=counts
+    dict_file = os.path.join(PATH, 'data/{}.tar.gz'.format(lang))        
+    if update and os.path.exists(dict_file):
+        old_dict = load_from_tar(dict_file)
+        for k in counts.keys():
+            try: old_dict[k]+=counts[k] #increment 
+            except KeyError: old_dict[k]=counts[k] #append
+        counts={}
+        freq_dict=old_dict          
+    save_dictionary(freq_dict, dict_file)
 
 class Speller:
     """ Main class that loads the dicitonary based on the language.
@@ -43,10 +89,13 @@ class Speller:
 
     def __init__(self, lang='en', threshold=0):
         self.threshold = threshold
-        dic_file = os.path.join(PATH, 'data/{}.tar.gz'.format(lang))
-        self.nlp_data = load_from_tar(dic_file)
+        self.dict_file = os.path.join(PATH, 'data/{}.tar.gz'.format(lang))
+        self.nlp_data = load_from_tar(self.dict_file)
         self.lang = lang
+        if threshold > 0:
+            self.prune_dictionary(threshold)
 
+    def prune_dictionary(self, threshold):
         if threshold > 0:
             print('Original number of words: {}'.format(len(self.nlp_data)))
             self.nlp_data = {k: v for k, v in self.nlp_data.items() if v > threshold}
@@ -63,6 +112,15 @@ class Speller:
             return self.nlp_data[word]
         return 0
 
+    def remove_words(self, words_list, permanent=False):
+        for word in words_list:
+            try:
+                del self.nlp_data[word]                
+            except KeyError:
+                print("Word",word,"not found")
+        if permanent:    
+            save_dictionary(self.nlp_data, self.dict_file)
+    
     def candidates(self, word, max_suggestions=3, labels=False):
         """
         >>> Speller.candidates("gxt")
